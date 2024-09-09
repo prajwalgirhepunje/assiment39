7e59920e456731f52c3f141b716515972703c1e7
prajwalgirhepunje <prajwalgirhepunje12@gmail.com>
2024-09-09 15:23:52 +0530

    promt code file added

---
+++ src/hellouser.js
@@ -0,0 +1,37 @@
+import './HelloUser.css'
+ 
+ const MorningCSS={
+    backgroundColor:"blue",
+    color:"black",
+    margin:"10px 0px",
+ }
+
+
+function HelloUser(props){
+     return(
+        <div className='first-container'>
+            Hello {props.name}
+        </div>
+     )
+}
+export default HelloUser
+
+
+function GoodMoring(props){
+    return(
+        <div style={MorningCSS}>
+            GoodMoring {props.name}
+        </div>
+    )
+}
+function GoodEvening(props){
+    return(
+        <div style={{
+            backgroundColor:"black",
+            color:"white",
+        }}>
+            GoodEvening {props.name}
+        </div>
+    )
+}
+export {GoodEvening,GoodMoring}
